fd7cc52401b73f0d710681764ae70928e4f868ae
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2022-05-28 11:05:51 -0700

    add: test_pythag, long and short

---
+++ more-wrappers.scm
@@ -0,0 +1,32 @@
+
+;;;; from TheReasonedSchemer/mk.scm
+
+(define-syntax lambdag@
+  (syntax-rules ()
+    ((_ (s) e) (lambda (s) e))))
+
+(define-syntax lambdaf@
+  (syntax-rules ()
+    ((_ () e) (lambda () e))))
+
+
+;;; generalized from microKanren/miniKanren-wrappers.scm reify-1st
+(define (reify-var iv s/c)         ;; iv=#(2) 
+  (let* ((v (walk* iv (car s/c)))  ;; v=(#(9) #(12) . #(13)) 
+	 (tmp (reify-s v '())))    ;; tmp=((#(13) . _.2) (#(12) . _.1) (#(9) . _.0)) 
+    (walk* v tmp)))                ;; x=(_.0 _.1 . _.2)
+
+#| trace-var-list takes a format string and a list of microKanren variables
+  Since printf is a macro, we can't:
+
+    (apply printf (cons fm xs))
+
+  so instead of absorbing the complexity of turning trace-var inself into
+  a macro, we restrict to having only one argument in each format string.
+|#
+(define trace-var-list
+  (lambda (fmt ivs)
+    (lambda (s/c)
+      (let ((xs (map (lambda (iv) (reify-var iv s/c)) ivs)))
+	(printf fmt xs)
+	(unit s/c)))))
---
+++ test_pythag_short.scm
@@ -0,0 +1,27 @@
+(load "microKanren/microKanren.scm")
+(load "microKanren/miniKanren-wrappers.scm")
+(load "more-wrappers.scm")
+(load "faster-miniKanren/numbers.scm")
+
+(define +o pluso)
+(define -o minuso)
+
+;;; find pythagorean triples - fixed?
+(time
+ (pretty-print
+  (run 3 (A)
+       (fresh (a b c a2 b2 c2)
+	      (== A (list a b c))
+	      (<o (build-num 0) a)     ; zeros don't count
+	      (<o (build-num 0) b)
+	      ;;(<o (build-num 0) c)   ; do we need to hint that c is an integer?
+	      (<o a b)                 ; optional perf hack: break symmetry
+	      (<o c (build-num 14))
+	      (*o a a a2)
+	      (*o c c c2)
+	      ;;(trace-var-list "considering (a b c)=~a\n" (list a b c))
+	      (+o a2 b2 c2)
+	      (*o b b b2)
+	      ))))
+
+
